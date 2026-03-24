28775ddf4ffe4e5823ccce0c36e780ab15fe2162
JunioHGD <henrique.goncalves.gd@gmail.com>
2026-03-24 16:15:17 -0300

    Adding index.js souce code into the repository.
    Implement order validation logic including age, payment, and discount checks.

--- Exercise 2 — Online Order Validation System/index.js
+++ Exercise 2 — Online Order Validation System/index.js
@@ -1,1 +1,73 @@
+//Order - These values are examples
+
+let productPrice = 15;
+
+let quantity = 20;
+
+let customerAge = 30;
+
+let hasAccount = true;
+
+let paymentMethod = "pix";  // "credit", "debit", "pix"
+
+let availableBalance = 1000;
+
+let productRestricted = false;
+
+//Calculate Order Value
+
+let totalPrice = productPrice * quantity;
+
+const discount = 0.1;
+
+if(totalPrice >= 300)
+    {
+         totalPrice -= (discount * totalPrice); 
+    }
+
+//Age Restriction
+let productAgeValidation = true;
+
+if((productRestricted) && (customerAge < 18))
+    {
+        productAgeValidation = false;
+    }
+
+//Payment Validation
+
+let paymentMethodValidation = false;
+
+if(paymentMethod == 'pix')
+    {
+        paymentMethodValidation = true;
+    }
+else if(((paymentMethod == 'credit') || (paymentMethod == 'debit')) && (availableBalance >= totalPrice))
+    {
+        paymentMethodValidation = true;
+    }
+else
+    {
+        console.log("Payment rejected.");
+    }
+
+//Final Result
+
+let orderApproved = false;
+
+if((productAgeValidation) && (hasAccount) && (paymentMethodValidation))
+    {
+        orderApproved = true;
+    }
+else
+    {
+        console.log("Order cannot proceed");
+    }
+
+if(orderApproved)
+    {
+        console.log("Order: " + orderApproved);
+        console.log("Total Price: " + Number(totalPrice.toFixed(2)));
+        console.log("Balance: " + availableBalance);
+        console.log("Payment Method: " + paymentMethod);
+    }
 
